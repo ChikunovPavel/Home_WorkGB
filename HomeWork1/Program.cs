45fc664d91970fb4d66542b93afd0fec4a3d87d2
Chikunov <Pasist@mail.ru>
2022-10-16 23:23:22 +0300

    Домашнее задание к первому семинару Чикунов Павел

--- HomeWork1/Program.cs
+++ HomeWork1/Program.cs
@@ -1,2 +1,86 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+/*
+
+Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine( ));
+Console.Write("Введите второе число:"); 
+int num2 = Convert.ToInt32(Console.ReadLine( ));
+
+if (num1 < num2)
+{
+    Console.WriteLine($"{num1} меньше чем {num2}");
+}
+
+else 
+{
+    Console.WriteLine($"{num1} больше чем {num2}");
+}
+
+*/
+
+//Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+/*
+
+Console.Write("Введите первое число:");
+int num1 = Convert.ToInt32(Console.ReadLine( ));
+Console.Write("Введите второе число:"); 
+int num2 = Convert.ToInt32(Console.ReadLine( ));
+Console.Write("Введите третье число:"); 
+int num3 = Convert.ToInt32(Console.ReadLine( ));
+
+int max = 0;
+
+if (num1 > max) max = num1;
+
+if (num2 > max) max = num2;
+
+if (num3 > max) max = num3;
+
+
+    Console.WriteLine("максимальное число:");
+    Console.WriteLine(max);
+
+*/
+
+//Задача 6: Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+/*
+
+Console.Write("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num % 2 == 0)
+{
+Console.Write($"{num} четное");
+}
+else
+{ 
+Console.Write($"{num} Не четное");
+}
+
+*/
+
+//Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+/*
+Console.Write("Введите число:");
+int N = Convert.ToInt32(Console.ReadLine());
+
+int Current = 1;
+
+while(Current <= N)
+
+    
+    if (Current % 2 == 0)
+    {
+     Console.Write(Current + " " );
+     Current++;
+            
+    }
+    else
+    {
+      Current++;
+    }
+*/    
+
